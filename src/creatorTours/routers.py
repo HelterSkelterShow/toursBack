68098ece011bb1d9c723e8fa38d302461c615c92
HelterSkelterShow <r.sergey2001@ya.ru>
2024-03-13 22:42:03 +0300

    #86 edit models for public tours

--- src/creatorTours/routers.py
+++ src/creatorTours/routers.py
@@ -291,7 +291,7 @@ async def publicDelete(id: str, user: User = Depends(current_user), session: Asy
 async def publicGetList(year: int, user: User = Depends(current_user), session: AsyncSession = Depends(get_async_session)) -> dict:
     try:
         stmt = tour_schema.join(tours_plan, tour_schema.c.tourId == tours_plan.c.schemaId)
-        query = stmt.select().with_only_columns(tour_schema.c.tourId, tours_plan.c.id.label('publicTourId'), tour_schema.c.tourName, tours_plan.c.price, tours_plan.c.meetingPoint,tours_plan.c.meetingDatetime,
+        query = stmt.select().with_only_columns(tour_schema.c.tourId, tours_plan.c.id.label('publicTourId'), tour_schema.c.tourName, tours_plan.c.price.label('tourAmount'), tours_plan.c.meetingPoint,tours_plan.c.meetingDatetime,
                                                 tours_plan.c.maxPersonNumber, tours_plan.c.dateFrom,tours_plan.c.dateTo, tours_plan.c.state)\
             .filter((tours_plan.c.dateTo > datetime.datetime(year - 1, 1, 1)) & (tours_plan.c.dateFrom < datetime.datetime(year + 2, 1, 1)) & (tours_plan.c.state != "cancelled"))
         result = await session.execute(query)
